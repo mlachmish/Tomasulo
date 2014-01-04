88f9682d6f03da08e3dd24d12564a754c4ae51fe
Guy Fliess <Guyfliess@gmail.com>
2014-01-04 17:37:50 +0200

    Jump

--- TomasuloSimulator/src/Sim.java
+++ TomasuloSimulator/src/Sim.java
@@ -34,22 +34,27 @@ public class Sim {
 
 		int pc = 0;
 		int instructionumber = 0;
-		Instruction currentInstruction;
+		Instruction currentInstruction = null;
 		Boolean waitingToIssue = true;
+		Boolean issued = true;
 		while (true) {
 
 			// "decode"
-			currentInstruction = instructionQueue.poll();
-			
-
+			if (issued) {
+				currentInstruction = instructionQueue.poll();
+			}
 			// Fetch
 			instructionQueue.add(memory.getInstruction(pc++));
-			
+
 			if (currentInstruction == null) {
 				// must be first Instruction
 				continue;
 			}
 			currentInstruction.setInstructionNumber(instructionumber++);
+			issued = false;
+
+			// traces
+			traces.add(currentInstruction);
 
 			// issue : check for branch, check if available RS
 			Constatns.Opcode opcode = currentInstruction.getOpcode();
@@ -59,40 +64,59 @@ public class Sim {
 			if (opcode == Constatns.Opcode.BEQ
 					|| opcode == Constatns.Opcode.BNE
 					|| opcode.equals(Constatns.Opcode.JUMP)) {
+
 				// Jump if possible
-				
-				traces.add(currentInstruction);
+				if (opcode != Constatns.Opcode.JUMP) {
+					Register SRC0 = intRegistersContainer
+							.getRegister(currentInstruction.getSRC0());
+					Register SRC1 = intRegistersContainer
+							.getRegister(currentInstruction.getSRC1());
+					if (SRC0.getState() == Constatns.State.Queued
+							|| SRC1.getState() == Constatns.State.Queued) {
+						continue;
+					}
+					if ((opcode == Constatns.Opcode.BEQ)
+							&& (SRC0.getData() == SRC1.getData())) {
+						issued = true;
+						continue;
+					}
+					if ((opcode == Constatns.Opcode.BNE)
+							&& (SRC0.getData() != SRC1.getData())) {
+						issued = true;
+						continue;
+					}
+				}
+				// JUMP!!!
+				pc += currentInstruction.getIMM();
+				currentInstruction = null;
+				issued = true;
 				continue;
 			}
 			//
 
-			// traces
-			traces.add(currentInstruction);
-			continue;
-			// Boolean issued =
-			// reservationStationContainer.issueInstraction(currentInstruction);
-			//
-			// if (issued)
-			// {
-			// instructionQueue.poll();
-			// }
-			//
-			// // Execute
-			// reservationStationContainer.excecute();
-			// // Write to CDB?
-			// //reservationStationContainer.updateQueuedFromCDB();
-			//
-			// Clock.incClock();
+			// continue;
+			issued = reservationStationContainer
+					.issueInstraction(currentInstruction);
+
+			if (issued) {
+
+			}
+
+			// Execute
+			reservationStationContainer.excecute();
+			// Write to CDB?
+			// reservationStationContainer.updateQueuedFromCDB();
+
+			Clock.incClock();
 
 		}
 
 		// Outputs
 		try {
 			Parser.createMemout(args[2]);
-		
-		Parser.createRegint(args[3]);		
-		Parser.createRegout(args[4]);
-		Parser.createTrace(args[5]);
+			Parser.createRegint(args[3]);
+			Parser.createRegout(args[4]);
+			Parser.createTrace(args[5]);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
